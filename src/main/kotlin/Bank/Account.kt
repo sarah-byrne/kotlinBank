ba9989be7ba7b847b2bde5768cbb761145b58974
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-04-11 15:05:13 +0100

    add deposit funcion to Account

--- src/main/kotlin/Bank/Account.kt
+++ src/main/kotlin/Bank/Account.kt
@@ -1,7 +1,20 @@
 package Bank
 
 class Account {
+    private var balance : Double = 0.0
+    private var transactions = mutableListOf<Transaction>()
+
     fun printStatement() : String {
-        return "date || credit || debit || balance"
+        var statement =  "date || credit || debit || balance"
+        transactions.forEach {
+            statement += " ${it.date} || ${it.amount} || || ${it.resultingBalance}"
+        }
+        return statement
+    }
+
+    fun deposit(amount : Double) {
+        balance += amount
+        val trans = Transaction(amount, true, balance)
+        transactions.add(trans)
     }
 }
--- src/test/kotlin/Bank/AccountTest.kt
+++ src/test/kotlin/Bank/AccountTest.kt
@@ -2,6 +2,7 @@ package Bank
 
 
 import org.junit.Test
+import java.time.LocalDate
 import kotlin.test.assertEquals
 
 class AccountTest {
@@ -11,10 +12,11 @@ class AccountTest {
         assertEquals("date || credit || debit || balance", account.printStatement())
     }
 
-//    @Test
-//    fun `Account records a transaction when a deposit is made`() {
-//        val account = Account()
-//        account.deposit(1.0)
-//        assertEquals("date || credit || debit || balance", account.printStatement())
-//    }
+    @Test
+    fun `Account records a transaction when a deposit is made`() {
+        val today = LocalDate.now()
+        val account = Account()
+        account.deposit(1.0)
+        assertEquals("date || credit || debit || balance ${today} || 1.0 || || 1.0", account.printStatement())
+    }
 }
